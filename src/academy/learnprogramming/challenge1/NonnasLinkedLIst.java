5ba881263c53e7fdfaeb945d6ddcfbf119fa24db
Nonna, Dzhabieva <nonna.dzhabieva@mckesson.com>
2021-02-10 16:28:27 -0700

    LinkedList Partially implemented

---
+++ src/academy/learnprogramming/challenge1/NonnasLinkedLIst.java
@@ -0,0 +1,36 @@
+package academy.learnprogramming.challenge1;
+
+public class NonnasLinkedLIst<E> {
+    private int size = 0;
+    private NonnasNode<E> node;
+    private NonnasNode<E> previous;
+    private NonnasNode<E> next;
+    private NonnasNode<E> head;
+
+    public NonnasLinkedLIst() {
+    }
+
+
+    public void add(E elem){
+        if(size == 0){
+            this.head = new NonnasNode<>(null, elem, null);
+            this.previous = head;
+            this.next = null;
+        }else{
+            this.node = new NonnasNode<>(previous, elem, next);
+            this.previous = node;
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        return "NonnasLinkedLIst{" +
+                "size=" + size +
+                ", node=" + node +
+                ", previous=" + previous +
+                ", next=" + next +
+                ", head=" + head +
+                '}';
+    }
+}
---
+++ src/academy/learnprogramming/challenge1/NonnasNode.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming.challenge1;
+
+public class NonnasNode<E> {
+    private E element;
+    private NonnasNode<E> next;
+    private NonnasNode<E> prev;
+
+    public NonnasNode(NonnasNode<E> prev, E element, NonnasNode<E> next) {
+        this.element = element;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+
+    @Override
+    public String toString() {
+        return "NonnasNode{" +
+                "element=" + element + '}';
+    }
+}
